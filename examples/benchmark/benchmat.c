ceb1aa55080a42de828950ed1271ae8962656f00
ecdr <Github.0hv@gishpuppy.com>
2014-08-31 14:58:48 -0700

    benchmat - test load with multiple panels

--- examples/benchmark/benchmat.c
+++ examples/benchmark/benchmat.c
@@ -4,6 +4,14 @@
 #include <Adafruit_GFX.h>
 #include <RGBmatrixPanel.h>
 
+#include "inc/hw_timer.h"
+#include "driverlib/timer.h"
+
+// FIXME: These should be based on matrix panel config
+#define TIMER_BASE WTIMER4_BASE
+#define TIMER_AB  TIMER_A
+
+
 #if defined(__AVR__)
 #define CLK 8  // MUST be on PORTB! (Use pin 11 on Mega)
 #define LAT A3
@@ -79,7 +87,7 @@
 
 #define NUM_LOOPS 0xFFFFFFFFUL
 
-RGBmatrixPanel RGBmatrix(A, B, C, D, CLK, LAT, OE, false, 1);
+//RGBmatrixPanel RGBmatrix(A, B, C, D, CLK, LAT, OE, false, 1);
 
 // Do something and time how long it takes, report the result
 unsigned long runbench(void) {
@@ -122,53 +130,96 @@ unsigned long runbench(void) {
   return time;
 }
 
+#define PANEL_MAX  8
 
 void setup() {
+  unsigned i;
 //  EnableTiming();
   pinMode(LEDPIN, OUTPUT);
   Serial.begin(UART_SPEED);
   
   unsigned long t_nodisp, t_disp;
 
+  Serial.println("***********************************");
+  Serial.println("RGBmatrixPanel benchmark begin");
+  Serial.println();
+
   Serial.print("No display: ");
   t_nodisp = runbench();
   Serial.println(t_nodisp);
-  RGBmatrix.begin();
-  delay(100);      // Short delay to avoid startup timing on RGBmatrix
 
-  Serial.println();
-  Serial.print("Display: ");
-  t_disp = runbench();
-  Serial.println(t_disp);
-  Serial.print("Difference: ");
-  Serial.println(t_disp - t_nodisp);
-  delay(100);      // Short delay to avoid startup timing on RGBmatrix
-  Serial.print("Slow refresh: ");
-  Serial.println(RGBmatrix.setRefresh(70));
-  Serial.print("Display: ");
-  t_disp = runbench();
-  Serial.println(t_disp);
-  Serial.print("Difference: ");
-  Serial.println(t_disp - t_nodisp);
-  Serial.println();
-
-  Serial.print("More colors: ");
-  Serial.println(RGBmatrix.setRefresh(140));
-  Serial.print("Display: ");
-  t_disp = runbench();
-  Serial.println(t_disp);
-  Serial.print("Difference: ");
-  Serial.println(t_disp - t_nodisp);
-  Serial.println();
+#if defined(DEBUG_RGBMAT)
+  Serial.println("RGBMatrix Debugging on");
+#endif
+#if defined(BENCHMARK_RGBMAT)
+  Serial.println("RGBMatrix Timing Measurement on");
+#endif
   
-  Serial.print("Fastest refresh: ");
-  Serial.println(RGBmatrix.setRefresh(2000));
-  Serial.print("Display: ");
-  t_disp = runbench();
-  Serial.println(t_disp);
-  Serial.print("Difference: ");
-  Serial.println(t_disp - t_nodisp);
+  for( i = 1; i <= PANEL_MAX; i++){
+      Serial.println("_______");
+      Serial.print("Panels: ");
+      Serial.println(i);
+      Serial.println();
+      RGBmatrixPanel LEDmatrix(A, B, C, D, CLK, LAT, OE, false, i);
+    
+      LEDmatrix.begin();
+      delay(100);      // Short delay to avoid startup timing on RGBmatrix
+      if(LEDmatrix.isRunning())
+        Serial.print("Running");
+      else
+        Serial.println("Refresh halted");
+      
+      Serial.println();
+      Serial.print("Display: ");
+      t_disp = runbench();
+      Serial.println(t_disp);
+      Serial.print("Difference: ");
+      Serial.print(t_disp - t_nodisp);
+      Serial.print(" % ");
+      Serial.println((float)(t_disp - t_nodisp)/t_nodisp);
+      delay(100);      // Short delay to avoid startup timing on RGBmatrix
+      Serial.print("Slow refresh: ");
+      Serial.println(LEDmatrix.setRefresh(70));
+      Serial.print("Display: ");
+      t_disp = runbench();
+      Serial.println(t_disp);
+      Serial.print("Difference: ");
+      Serial.print(t_disp - t_nodisp);
+      Serial.print(" % ");
+      Serial.println(((float)t_disp - t_nodisp)/t_nodisp);
+      Serial.println();
+    
+      Serial.print("Faster refresh: ");
+      Serial.println(LEDmatrix.setRefresh(140));
+      Serial.print("Display: ");
+      t_disp = runbench();
+      Serial.println(t_disp);
+      Serial.print("Difference: ");
+      Serial.print(t_disp - t_nodisp);
+      Serial.print(" % ");
+      Serial.println(((float)t_disp - t_nodisp)/t_nodisp);
+      Serial.println();
+      
+      Serial.print("Fastest refresh: ");
+      Serial.println(LEDmatrix.setRefresh(2000));
+      delay(1);  // Be sure the refresh has time to take effect
+      if(LEDmatrix.isRunning())
+        Serial.print("Running");
+      else
+        Serial.println("Refresh halted");
+      
+      Serial.print("Display: ");
+      t_disp = runbench();
+      Serial.println(t_disp);
+      Serial.print("Difference: ");
+      Serial.print(t_disp - t_nodisp);
+      Serial.print(" % ");
+      Serial.println(((float)t_disp - t_nodisp)/t_nodisp);
+      Serial.println();
+      LEDmatrix.end();
+  }
   Serial.println();
+  Serial.println("Benchmark end");
  // If optimize driver to handle blank lines specially, then would be interesting to benchmark
  // with something on screen vs. blank screen.
  //  drawstuff();
@@ -178,4 +229,3 @@ void setup() {
 void loop() {
 
 }
-
